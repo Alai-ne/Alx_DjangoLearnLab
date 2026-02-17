8505f9f2cc1ab4b31cd3e83441afbe7bcf3b754b
Alai-ne <alaineakeza12@gmail.com>
2026-02-17 04:11:58 -0800

    save

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render, get_object_or_404
+# Create your view here
 from django.views.generic import DetailView
 from .models import Book, Library
 
